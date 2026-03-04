e7e9621dd5928b533730fb4bb9f4864541184f48
Roman <roman.i61@yandex.com>
2026-03-04 06:19:57 +0300

    done

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,50 @@
 package handlers
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "ошибка генерации страницы", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(file)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20) // 10 MB
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Ошибка при чтении файла", http.StatusInternalServerError)
+		return
+	}
+	converted := service.Convert(string(fileBytes))
+
+	if err := os.Mkdir("uploads", 0755); err != nil && !errors.Is(err, os.ErrExist) {
+		log.Fatal(err)
+	}
+
+	fileName := filepath.Join("uploads", time.Now().UTC().String()+filepath.Ext(handler.Filename))
+	os.WriteFile(fileName, []byte(converted), 0755)
+
+	io.WriteString(w, converted)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,20 @@
 package service
+
+import "github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+
+func isMorse(s string) bool {
+	for _, ch := range s {
+		if ch != '.' && ch != '-' && ch != ' ' {
+			return false
+		}
+	}
+	return true
+}
+
+func Convert(s string) string {
+	isMorse := isMorse(s)
+	if isMorse {
+		return morse.ToText(s)
+	}
+	return morse.ToMorse(s)
+}
